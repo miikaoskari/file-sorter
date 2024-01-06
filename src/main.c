8fc22c7c2d2d0551cf6d5bf030f32599666567b7
miikaoskari <miika.karjalainen13@gmail.com>
2024-01-06 23:49:13 +0200

    Simple window implementation

---
+++ src/main.c
@@ -0,0 +1,25 @@
+//
+// Created by miika on 1/6/24.
+//
+#include <gtk/gtk.h>
+
+static void activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "File Sorter");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_window_present(GTK_WINDOW(window));
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("org.miika.file-sorter", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
+}
