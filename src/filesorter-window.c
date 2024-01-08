ad577953d321d787e2ba7528608fb1615c936c27
miikaoskari <miika.karjalainen13@gmail.com>
2024-01-08 19:48:11 +0200

    Add drag and drop

--- src/filesorter-window.c
+++ src/filesorter-window.c
@@ -22,38 +22,71 @@
 
 #include "filesorter-window.h"
 
-struct _FilesorterWindow
-{
-	AdwApplicationWindow  parent_instance;
+struct _FilesorterWindow {
+    AdwApplicationWindow parent_instance;
 
-	/* Template widgets */
-	AdwHeaderBar        *header_bar;
-	GtkLabel            *label;
-        GtkTextView *main_text_view;
-        GtkButton *open_button;
+    /* Template widgets */
+    AdwHeaderBar *header_bar;
+    GtkLabel *label;
+    GtkTextView *main_text_view;
+    GtkButton *open_button;
 };
 
 G_DEFINE_FINAL_TYPE (FilesorterWindow, filesorter_window, ADW_TYPE_APPLICATION_WINDOW)
 
 
+static void filesorter_window_class_init(FilesorterWindowClass *klass) {
+    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-
-
-static void
-filesorter_window_class_init (FilesorterWindowClass *klass)
-{
-	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
-
-	gtk_widget_class_set_template_from_resource (widget_class, "/org/miika/filesorter/filesorter-window.ui");
-	gtk_widget_class_bind_template_child (widget_class, FilesorterWindow, header_bar);
-	gtk_widget_class_bind_template_child (widget_class, FilesorterWindow, label);
-        gtk_widget_class_bind_template_child (widget_class, FilesorterWindow, main_text_view);
-        gtk_widget_class_bind_template_child (widget_class, FilesorterWindow, open_button);
+    gtk_widget_class_set_template_from_resource(widget_class, "/org/miika/filesorter/filesorter-window.ui");
+    gtk_widget_class_bind_template_child (widget_class, FilesorterWindow, header_bar);
+    gtk_widget_class_bind_template_child (widget_class, FilesorterWindow, label);
+    gtk_widget_class_bind_template_child (widget_class, FilesorterWindow, main_text_view);
+    gtk_widget_class_bind_template_child (widget_class, FilesorterWindow, open_button);
 }
 
-static void
-filesorter_window_init (FilesorterWindow *self)
-{
-	gtk_widget_init_template (GTK_WIDGET (self));
+static gboolean on_drop(GtkDropTarget *target,
+                        const GValue *value,
+                        double x,
+                        double y,
+                        gpointer data) {
+    /* GdkFileList is a boxed value, so we use the boxed API. */
+    GdkFileList *file_list = g_value_get_boxed(value);
+
+    GSList *list = gdk_file_list_get_files(file_list);
+
+    /* Loop through the files and print their names. */
+    for (GSList *l = list; l != NULL; l = l->next) {
+        GFile *file = l->data;
+        g_print("%s\n", g_file_get_path(file));
+    }
+
+    return TRUE;
+}
+
+static GdkDragAction on_enter(GtkDropTarget *target,
+                              double x,
+                              double y,
+                              gpointer data) {
+    /* Custom code... */
+
+    /* Tell the callee to continue. */
+    return GDK_ACTION_COPY;
+}
+
+static void on_leave(GtkDropTarget *target,
+                     gpointer data) {
+    /* Custom code... */
+}
+
+static void filesorter_window_init(FilesorterWindow *self) {
+    gtk_widget_init_template(GTK_WIDGET (self));
+
+    GtkDropTarget *target = gtk_drop_target_new(G_TYPE_INVALID, GDK_ACTION_COPY);
+    gtk_drop_target_set_gtypes(target, (GType[1]) {GDK_TYPE_FILE_LIST,}, 1);
+    g_signal_connect (target, "drop", G_CALLBACK(on_drop), NULL);
+    g_signal_connect (target, "enter", G_CALLBACK(on_enter), self);
+    g_signal_connect (target, "leave", G_CALLBACK(on_leave), self);
+    gtk_widget_add_controller(GTK_WIDGET (self), GTK_EVENT_CONTROLLER (target));
 }
 
